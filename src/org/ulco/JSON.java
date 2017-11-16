fc7ab8b275c450bc84bebcac3a9ced69d7e3c750
simon <herlin.simon@gmail.com>
2017-11-16 13:50:50 +0100

    Recfatoring second task ok

--- src/org/ulco/JSON.java
+++ src/org/ulco/JSON.java
@@ -1,17 +1,27 @@
 package org.ulco;
-
+import java.lang.reflect.*;import java.lang.reflect.*;
 public class JSON {
+
     static public GraphicsObject parse(String json) {
         GraphicsObject o = null;
         String str = json.replaceAll("\\s+", "");
         String type = str.substring(str.indexOf("type") + 5, str.indexOf(","));
-
-        if (type.compareTo("square") == 0) {
-            o = new Square(str);
-        } else if (type.compareTo("rectangle") == 0) {
-            o = new Rectangle(str);
-        } else if (type.compareTo("circle") == 0) {
-            o = new Circle(str);
+        String typeClass = type.substring(0, 1).toUpperCase() + type.substring(1);
+        Class classe=null;
+        try {
+            classe = Class.forName("org.ulco."+typeClass);
+            Constructor constructeur = classe.getConstructor(String.class);
+            o = (GraphicsObject) constructeur.newInstance(str);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
         return o;
     }
